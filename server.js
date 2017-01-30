550eb35a5fa0e2ebffd86f7bafb3bafad873a175
inthrakumar <inthrakumar@gmail.com>
2017-01-31 03:40:39 +0530

    Add projects page

--- server.js
+++ server.js
@@ -45,6 +45,13 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects page',
+        pageMessage: 'Welcome to My Projects'
+    })
+});
+
 app.get('/bad', (req, res) => {
     res.send({
                 errorMessage: 'Unable to process your request'       
